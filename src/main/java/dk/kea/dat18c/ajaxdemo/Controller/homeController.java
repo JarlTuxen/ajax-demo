cde0dfd354de0ecfe329b7fe66d23c5cee973720
JarlTuxen <jarl.tuxen@gmail.com>
2019-11-18 22:26:53 +0100

    first commit

---
+++ src/main/java/dk/kea/dat18c/ajaxdemo/Controller/homeController.java
@@ -0,0 +1,18 @@
+package dk.kea.dat18c.ajaxdemo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController
+{
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+
+    @GetMapping("/js_basics")
+    public String jsBasics() { return "js_basics"; }
+}
+
---
+++ src/test/java/dk/kea/dat18c/ajaxdemo/AjaxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.dat18c.ajaxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
